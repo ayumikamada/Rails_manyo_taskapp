c5d288e5c26898e3e2e697b4dd323e21334f999c
ayumi kamada <dmrdmr58@gmail.com>
2020-10-07 22:33:58 +0900

    CRUD機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,55 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all.order(created_at: :desc)
   end
 
   def show
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice: "投稿完了！"
+      else
+        render :new
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "編集完了！"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"削除完了！"
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
